518f7d1f2b9b0aa3fd62d2417cec84ee63b0e9ee
mirzahaifzh <mirzafio78@gmail.com>
2024-11-13 08:42:29 +0800

    fix

--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: "root",
-    password: "root",
-    database: "db_penjualan_sepatu",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
   test: {
-    username: "root",
-    password: "root",
-    database: "db_penjualan_sepatu",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: "root",
-    password: "root",
-    database: "db_penjualan_sepatu",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
 };
