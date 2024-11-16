c1fb2d19ae86330d715bf19f18bd26af52f896a4
mirzahaifzh <mirzafio78@gmail.com>
2024-11-16 16:25:04 +0800

    fix models

--- models/comment.js
+++ models/comment.js
@@ -1,11 +1,12 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const Comment = sequelize.define('Comment', {
     product_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'ProdukSepatu',
+        model: 'ProdukSepatu', // Pastikan nama model yang benar
         key: 'id',
       },
     },
@@ -13,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: true, // Allow anonymous comments
       references: {
-        model: 'User',
+        model: 'User', // Pastikan nama model yang benar
         key: 'id',
       },
     },
@@ -21,9 +22,13 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-  }, {});
+  }, {
+    tableName: 'comments', // Nama tabel yang sesuai dengan konvensi snake_case
+    modelName: 'Comment', // Nama model sesuai dengan PascalCase
+  });
 
   Comment.associate = function(models) {
+    // Definisikan asosiasi antara model
     Comment.belongsTo(models.ProdukSepatu, { foreignKey: 'product_id' });
     Comment.belongsTo(models.User, { foreignKey: 'user_id' });
   };
